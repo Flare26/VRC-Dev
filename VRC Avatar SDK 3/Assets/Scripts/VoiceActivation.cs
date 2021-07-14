54727b4c42a71ef650c0aaefdea2628d393e2283
Flare26 <47835828+Flare26@users.noreply.github.com>
2021-07-13 20:17:02 -0500

    main

---
+++ VRC Avatar SDK 3/Assets/Scripts/VoiceActivation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+public class VoiceActivation : MonoBehaviour
+{
+    Animator anim;
+    Renderer rend;
+    public float brightness = 6.66f;
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        anim = GetComponent<Animator>();
+        //VRC.SDK3.Avatars.Components.VRCAvatarDescriptor
+    }
+}
