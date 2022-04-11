ec7d88b185cc3d2633e06d8094a314fa84a37a2e
Flare26 <47835828+Flare26@users.noreply.github.com>
2022-04-11 01:37:34 -0500

    lots

---
+++ CyberBar - VRC/Assets/Scenes/PortalSetupPt2.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PortalSetupPt2 : UdonSharpBehaviour
+{
+
+    Transform playerCamera;
+    [SerializeField] GameObject ps;
+    void Start()
+    {
+        VRCPlayerApi p = Networking.LocalPlayer;
+        GameObject pcm = GameObject.Find("Player Camera");
+        ps.playerCamera = pcm.transform;
+
+    }
+}
